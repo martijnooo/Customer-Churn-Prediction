c418f7c3ca813df34344d93a26850da36d55c3d7
martijnooo <martijnbalder@gmail.com>
2025-11-21 11:44:06 +0000

    ingestion made for flexible

--- src/ingestion/ingestion.py
+++ src/ingestion/ingestion.py
@@ -4,12 +4,11 @@ from pathlib import Path
 
 # Determine project root based on this file's location
 PROJECT_ROOT = Path(__file__).resolve().parents[2]   # goes up from src/ingestion/
-RAW_DATA_DIR = PROJECT_ROOT / "data" / "1_raw"
+RAW_DATA_DIR = PROJECT_ROOT / "data" 
 
-def data_reader(file: str) -> pd.DataFrame:
-    file_path = RAW_DATA_DIR / f"{file}.csv"
+def data_reader(file: str, folder: str = "1_raw") -> pd.DataFrame:
+    file_path = RAW_DATA_DIR / folder / f"{file}.csv"
     return pd.read_csv(file_path)
 
-def load_all_data() -> Dict[str, pd.DataFrame]:
-    datasets = ["customers", "support_interactions", "transactions"]
-    return {name: data_reader(name) for name in datasets}
+def load_all_data(datasets = ["customers", "support_interactions", "transactions"], folder: str = "1_raw") -> Dict[str, pd.DataFrame]:
+    return {name: data_reader(name, folder) for name in datasets}
